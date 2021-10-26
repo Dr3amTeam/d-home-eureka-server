c566dd0505c11c9b922261d0acfe89552a104e78
DanielJaureguiGonzales <dank_lp123@outlook.com>
2021-10-26 12:26:52 -0500

    Add configuration of eureka

---
+++ src/test/java/com/dreamteam/dhome/eureka/DHomeEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamteam.dhome.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DHomeEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
